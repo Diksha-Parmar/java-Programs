6a1711ef46dbf605a5c5d9568a151be909d63b35
Diksha-Parmar <dikshaparmar750@gmail.com>
2023-06-19 19:53:42 +0530

    Constuctor

---
+++ packagedemo/packages.java
@@ -0,0 +1,21 @@
+package packagedemo;
+public class packages{
+	public void method(){
+			System.out.println(" In public method " );
+		}
+		protected void method2(){
+			System.out.println(" In protected method ");
+		}
+		public int d;
+		private int f;
+		protected int g;
+	public static void main(String args[]){
+		
+		
+		//private void method1(){
+			//System.out.println(" In private method ");
+		//}
+		
+	}
+}
+				
